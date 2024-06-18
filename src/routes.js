fe2d466a21ce68740b2131d3cf7cf73dbc18feac
ermeson119 <ermesonbalbinot119@gmail.com>
2024-06-18 10:47:41 -0300

    criando primeiro migration e criado model User

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './App/models/User'
+
 
 const routes = new Router()
 
-routes.get('/', (request, response) =>{
-    return response.status(200).json({message: 'Hello Word!'})
+routes.get('/', async (request, response) =>{
+    const user = await User.create({
+        id: v4(),
+        name: "Anna",
+        email: "Anna@gmail.com",
+        password_hash: '1154865',
+    })
+    return response.status(201).json(user)
 })
 
 export default routes
